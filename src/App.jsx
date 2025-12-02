6dc45dc8634ec928ae9efbf70d8c1fe1a3805fe6
Lazaros Drakopoulos <lazaros.drou@gmail.com>
2025-12-02 19:45:57 +0200

    add first compontet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-import ReactImg from './assets/react-core-concepts.png';
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import ReactImg from '../assets/react-core-concepts.png';
+import Header from './components/Header.jsx';
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={ReactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (
