4172bace5b83f6e355bd42a54fe799759a30cc2f
Lazaros Drakopoulos <lazaros.drou@gmail.com>
2025-12-03 17:08:41 +0200

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import ReactImg from '../assets/react-core-concepts.png';
+import ReactImg from './assets/react-core-concepts.png';
 import Header from './components/Header.jsx';
+import componentsImg from './assets/components.png';
+import CoreConcept from './components/CoreConcept.jsx'
 
 
 function App() {
@@ -7,7 +9,21 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!!</h2>
+        <section id='core-concepts'>
+         <h2>Core concept</h2>
+<ul>
+<CoreConcept 
+title = "components"
+description = "the core ui"
+image = {componentsImg}
+
+/>
+<CoreConcept />
+<CoreConcept />
+<CoreConcept />
+</ul>
+        </section>
+        
       </main>
     </div>
   );
