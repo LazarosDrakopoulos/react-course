ba8fc274c2fa77c961f72abea6ddd9eb8730411c
Lazaros Drakopoulos <lazaros.drou@gmail.com>
2025-12-06 08:59:30 +0200

    add event listeners

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import { CORE_CONCEPTS } from './data.js';
+import TabButton from './components/TabButton.jsx';
 
 export default function App() {
+     function Handleclick() {
+console.log("sssss");
+        }
   return (
     <div>
       <Header />
@@ -15,6 +19,16 @@ export default function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+            <h2>Examples</h2>
+            <menu>
+          <TabButton onSelect={handleSelect}>Components</TabButton>
+          <TabButton onSelect={handleSelect}>JSX</TabButton>
+          <TabButton onSelect={handleSelect}>Props</TabButton>
+          <TabButton onSelect={handleSelect}>State</TabButton>
+            </menu>
+            Dynamic
         </section>
       </main>
     </div>
