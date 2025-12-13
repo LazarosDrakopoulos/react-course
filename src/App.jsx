ba867e482ad0019040c07030818adea52378d8d1
Lazaros Drakopoulos <lazaros.drou@gmail.com>
2025-12-13 11:27:58 +0200

    apply new content

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import { CORE_CONCEPTS } from './data.js';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES} from './data.js'
 
 export default function App() {
-  const [selectedTopic , setSelectedTopic ] = useState('please click a button');
+  const [selectedTopic , setSelectedTopic ] = useState('components');
      function handleSelect(selectedButton) {
 setSelectedTopic(selectedButton);
         }
@@ -31,7 +32,13 @@ setSelectedTopic(selectedButton);
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
-            {selectedTopic}
+           <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+           </div>
         </section>
       </main>
     </div>
